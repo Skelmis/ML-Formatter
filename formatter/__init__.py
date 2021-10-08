eca3df22b8956cd64dad21990168ae0418d01906
skelmis <skelmis.craft@gmail.com>
2021-10-08 21:22:16 +1300

    Update versioning

--- formatter/__init__.py
+++ formatter/__init__.py
@@ -2,7 +2,9 @@ from collections import namedtuple
 
 from .deepspeech import DeepSpeech
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel="beta", serial=0)
+version_info = VersionInfo(
+    major=1, minor=0, micro=1, releaselevel="production", serial=0
+)
